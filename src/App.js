18fa09dac08f054b29a5ca987b62ae4debaa8297
taeeun kim <kimtaeeeny@gmail.com>
2020-07-07 16:56:37 +0900

    다시다시2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import "./App.css";
 import generator from "./utils/generator";
 
 function App() {
-  console.log(words);
   useEffect(() => {
     generator();
   }, []);
 
   return (
     <div id="App">
-      <div id="words">{words}</div>
+      <div id="words"></div>
     </div>
   );
 }
